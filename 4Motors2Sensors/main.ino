5406a28a256b2f7356fba103b4cebd6fc82ead49
Vinamra Yadav <73683711+MythicalMAxX@users.noreply.github.com>
2024-02-17 22:26:07 +0530

    Create main.ino

---
+++ 4Motors2Sensors/main.ino
@@ -0,0 +1,51 @@
+// Motor pin definitions
+#define MOTOR_FL_PIN 2 // Front-left motor
+#define MOTOR_FR_PIN 3 // Front-right motor
+#define MOTOR_BL_PIN 4 // Back-left motor
+#define MOTOR_BR_PIN 5 // Back-right motor
+
+// Sensor pin definitions
+#define SENSOR_LEFT_PIN A0
+#define SENSOR_RIGHT_PIN A1
+
+// Sensor threshold
+#define LINE_THRESHOLD 700 // Adjust as needed
+
+// Motor speed
+#define MOTOR_SPEED 200 // Adjust as needed
+
+void setup() {
+  // Set motor pins as output
+  pinMode(MOTOR_FL_PIN, OUTPUT);
+  pinMode(MOTOR_FR_PIN, OUTPUT);
+  pinMode(MOTOR_BL_PIN, OUTPUT);
+  pinMode(MOTOR_BR_PIN, OUTPUT);
+
+  // Set sensor pins as input
+  pinMode(SENSOR_LEFT_PIN, INPUT);
+  pinMode(SENSOR_RIGHT_PIN, INPUT);
+}
+
+void loop() {
+  // Read sensor values
+  int leftSensor = analogRead(SENSOR_LEFT_PIN);
+  int rightSensor = analogRead(SENSOR_RIGHT_PIN);
+
+  // Calculate error
+  int error = leftSensor - rightSensor;
+
+  // Adjust motor speeds based on error
+  int leftSpeed = MOTOR_SPEED + error;
+  int rightSpeed = MOTOR_SPEED - error;
+
+  // Apply motor speeds
+  // Front motors
+  analogWrite(MOTOR_FL_PIN, constrain(leftSpeed, 0, 255));
+  analogWrite(MOTOR_FR_PIN, constrain(rightSpeed, 0, 255));
+  // Back motors
+  analogWrite(MOTOR_BL_PIN, constrain(leftSpeed, 0, 255));
+  analogWrite(MOTOR_BR_PIN, constrain(rightSpeed, 0, 255));
+  
+  // Delay for stability
+  delay(10);
+}
